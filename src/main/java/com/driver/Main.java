3351db999289ce39dab45f5bd2d7f693a9995624
Anoop kushwaha <anoopkushwaha509@gmail.com>
2023-06-12 16:28:18 -0400

    method overriding in Java-2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,4 +8,22 @@ public class Main {
 
 //        return null;
     }
+
+    public static class B extends  A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
+
+
+    public static class A {
+        public String meth(){
+            return "Invoking method from class A";
+        }
+
+    }
+
 }
+
