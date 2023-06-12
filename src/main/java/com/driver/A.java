f9f06848675f285a942dd36293a901ceaa84c8bf
Anoop kushwaha <anoopkushwaha509@gmail.com>
2023-06-12 16:19:27 -0400

    method overriding in Java.

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public abstract class A {
+    static String meth(){
+        return "Invoking method from class A";
+    }
+
+}
---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+public class B extends  A{
+//@Override
+   static String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+
+}
